ae7df9bf78f8aad1a75848eff40dc0254b7ca57b
Thanav Krishna <thanavkrishna14@gmail.com>
2026-09-02 22:35:39 +0530

    Add registration form validation

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,1 +1,26 @@
 console.log("DocuVault authentication JavaScript loaded!");
+const form = document.querySelector("form");
+console.log(form);
+
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+
+const passwordError = document.getElementById("password-error");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    if(password.value !== confirmPassword.value)
+    {
+        passwordError.textContent = "Passwords do not match!";
+        console.log("Passwords do not match!");
+    }
+    else
+    {
+        passwordError.textContent = "";
+        console.log("Passwords match!");
+    }
+
+    console.log("Form was submitted!");
+
+});
